ab264a311e4ac90174102b6ebd136be1b2648875
Janne Karttunen <janne.a.karttunen@gmail.com>
2025-08-31 14:59:34 +0300

    Rebase from 'upstream'

--- turtle_nest/include/turtle_nest/package_generators/package_generator_factory.h
+++ turtle_nest/include/turtle_nest/package_generators/package_generator_factory.h
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------
- * Copyright 2024 Janne Karttunen
+ * Copyright 2025 Janne Karttunen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,21 +14,13 @@
  * limitations under the License.
  * ------------------------------------------------------------------
 */
+#ifndef PACKAGE_GENERATOR_FACTORY_H
+#define PACKAGE_GENERATOR_FACTORY_H
 
-#ifndef GENERATE_LAUNCH_H
-#define GENERATE_LAUNCH_H
+#include "turtle_nest/build_type_enum.h"
+#include "turtle_nest/package_generators/base_package_generator.h"
 
-#include <QString>
-#include <turtle_nest/node_type_enum.h>
+std::unique_ptr < BasePackageGenerator > create_package_generator(BuildType package_type);
 
 
-void generate_launch_file(
-  QString workspace_path, QString package_name, QString launch_file_name, QString params_file_name,
-  NodeType node_type, QString node_name = "");
-QString generate_launch_text(
-  QString package_name, QString node_name_cpp, NodeType node_type, QString params_file_name);
-void append_launch_to_cmake(QString c_make_path);
-void save_launch_file(QString file_name, QString launch_text);
-void append_launch_to_setup_py(QString setup_py_path, QString package_name);
-
-#endif // GENERATE_LAUNCH_H
+#endif // PACKAGE_GENERATOR_FACTORY_H
